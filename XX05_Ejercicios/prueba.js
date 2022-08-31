ac21c9e4df64895ebd9f5debe2efb249a2ef99e7
Sofia <sofisaspa@gmail.com>
2022-08-30 21:55:04 -0300

    20220830 v2

--- XX05_Ejercicios/prueba.js
+++ XX05_Ejercicios/prueba.js
@@ -14,33 +14,3 @@ function finDeSemana (dia) {
 }
 finDeSemana(dia)*/
 
-function totalAPagar(vehiculo,litrosConsumidos) {
-	let precioPorLitro;
-	let precioParcial;
-	let precioTotal;
-
-	if (vehiculo == "coche") {
-		precioPorLitro = 86;
-		precioTotal = precioPorLitro;
-			
-	}else if (vehiculo=="moto") {
-		precioPorLitro = 70;
-		precioTotal = precioPorLitro;
-		
-	} else if(vehiculo="autobus") {
-		precioPorLitro = 55;
-		precioTotal = precioPorLitro;
-	}
-	
-	if (litrosConsumidos<=25) {
-		precioParcial= precioPorLitro*litrosConsumidos;	
-		precioTotal=precioParcial+50;		
-		
-	} else {
-		precioParcial= precioPorLitro*litrosConsumidos;	
-		precioTotal = precioParcial+25;	
-		console.log("aca");	
-	}
-	return precioTotal;
-}
-console.log(totalAPagar("autobus",25));
