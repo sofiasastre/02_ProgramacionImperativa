d4daf61beb37a851bcc8623439bee14b12b2d206
Sofia <sofisaspa@gmail.com>
2022-09-07 20:55:45 -0300

    20220907 2255

---
+++ Modulo_3_Algoritmos/16A - Algoritmos de iteración y toma de decisiones - SIN TERMINAR/01_Ejercicio.js
@@ -0,0 +1,5 @@
+// Agreguemos más...
+// Ahora vamos a refactorizar nuestro código con la siguiente indicación.
+
+// Dados 4 números reales (enteros, decimales, positivos y/o negativos), ¿cómo podemos identificar el número mayor y mostrarlo por la consola? Modifiquemos el código para que ahora acepte 4 números.
+
---
+++ Modulo_3_Algoritmos/16A - Algoritmos de iteración y toma de decisiones - SIN TERMINAR/02_Ejercicio.js
@@ -0,0 +1,56 @@
+//EJERCICIO 1
+// Dado los siguientes numeros:
+
+let num1 = 43;
+let num2 = 78;
+let num3 = 14;
+
+// Crea un algoritmo que te permita identificar
+// el numero mayor. Utiliza condicionales como el
+// if, else, o else if.
+
+// Escribe tu código debajo de estas lineas,
+// y observa los resultados por la consola.
+// especificamente para este conjunto de datos
+// deberias ver por consola el numero 78,
+// adicional, cambia los valores de num1, num2 y num3
+// para verificar que tu algoritmo funcione con
+// cualquiera 3 valores.
+
+
+
+//EJERCICIO 2
+// Ahora bien. dado los siguientes numeros:
+
+let num4 = 13;
+let num5 = 1663;
+let num6 = 3363;
+// agrega una variable mas, llamada num4
+// con un valor numerico
+
+// Crea un algoritmo que te permita identificar
+// el numero mayor de los 4 anteriores.
+
+// Escribe tu código debajo de esta linea,
+// y observa los resultados por la consola.
+// Adicional, cambia los valores de los 4 numeros
+// para ver que algoritmo funciona con cualquier
+// conjunto de 4 numeros.
+
+
+//EJERCICIO 3
+/**
+ * Extra: que puedes hacer para identificar
+ * el numero mayor de una lista de numeros,
+ * como la siguiente? Utiliza Javascript.
+ *
+ * Tip: es valido buscar en google :)
+ *
+ * Adicional: al terminar tu algoritmo,
+ * cambia los valores de la lista, bien sea
+ * agregando o removiendo, y asegurate
+ * que tu algoritmo funcione con cualquier lista
+ * de numeros
+ */
+
+ const numeros = [5, 12, 2, 40, 33, 2, 8];
