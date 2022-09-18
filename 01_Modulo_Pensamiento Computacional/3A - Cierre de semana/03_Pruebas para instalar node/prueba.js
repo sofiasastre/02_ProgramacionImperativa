0c823bea9ff46fd2d8c08d10c4da23398b1ded76
Sofia <sofisaspa@gmail.com>
2022-09-18 18:36:37 -0300

    20220918 1834

---
+++ 01_Modulo_Pensamiento Computacional/3A - Cierre de semana/03_Pruebas para instalar node/prueba.js
@@ -0,0 +1,14 @@
+let cajonRemeras = "pantalones";
+let cajonPantalones = "remeras";
+let cama;
+
+console.log("Bienvenidos");
+
+function sumar(a, b) {
+  return a + b;
+}
+let c;
+
+c = sumar(5, 4);
+console.log(c);
+
